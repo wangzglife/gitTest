72b3ac40e5a898d3ea6600d761fd08bce01269ce
simon <wangzglife@163.com>
2018-01-28 18:52:33 +0800

    初始化的版本

--- src/main/java/com/dblued/controller/MainController.java
+++ src/main/java/com/dblued/controller/MainController.java
@@ -1,7 +1,5 @@
 package com.dblued.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
