3431f0ee70a49437861f6c65acc32415b860d06c
simon <wangzglife@163.com>
2018-01-28 18:04:39 +0800

    initial commit

---
+++ src/main/java/com/dblued/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.dblued.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    
+}
